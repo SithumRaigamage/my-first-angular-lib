d70cbd62d4ac41ced2228ffcc6aff6ba5faf31e3
SithumRaigamage <sraig2002@gmail.com>
2024-09-22 13:30:28 +0530

    added lib to the package.json and displayed the library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularLibComponent } from "../../projects/angular-lib/src/lib/angular-lib.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularLibComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
